d397c08462048af859b66bba6f8af158dcc966bc
developer-shourav <mrinmoyraj823@gmail.com>
2025-03-30 16:21:20 +0600

    Base API and route setup completed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "./features/task/taskSlice";
-import userReducer from "./features/user/userSlice";
+
 
 export const store = configureStore({
-   reducer: {
-    todo: taskReducer,
-    user: userReducer,
-   },
+   reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
